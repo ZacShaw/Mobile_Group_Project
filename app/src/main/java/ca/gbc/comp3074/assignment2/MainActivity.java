37cecccd96b6ddeb08793581debe788e9145ab81
bestie06 <fatih.kgokmen@gmail.com>
2020-11-28 02:28:02 -0500

    Logo and navigation Bar
    I m still working for navigation bar,Couldnt implemented it yet

--- app/src/main/java/ca/gbc/comp3074/assignment2/MainActivity.java
+++ app/src/main/java/ca/gbc/comp3074/assignment2/MainActivity.java
@@ -1,17 +1,25 @@
 package ca.gbc.comp3074.assignment2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.state.State;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void about(View view) {
@@ -36,4 +44,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, introductionGame.class);
         startActivity(intent);
     }
+
+
 }
